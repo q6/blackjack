540f9684575ae40d6a033eaa1c4782a8b0a042d0
Oxen <larmannjan@gmail.com>
2015-02-17 16:47:50 +0100

    add Player class

--- blackjack.py
+++ blackjack.py
@@ -36,9 +36,57 @@ class Deck(object):
             lst.append(str(card))
         return '\n'.join(lst)
 
+    def remove(self, index):
+        try:  # user might want to remove card that isn't in deck
+            self.deck.pop(index)
+            return True
+        except IndexError:
+            return False
+
+class Player(object):
+
+    def __init__(self):
+        self.hand = []
+
+    def add_card_to_hand(self, card):
+        """
+        Assume card is a valid card object not in hand
+        """
+        self.hand.append(card)
+
+    def remove_card_from_hand(self, card):
+        """
+        Assume card is a valid card object in hand
+        """
+        self.hand.remove(card)
+
+    def clear_hand(self):
+        self.hand = []
+
 class Play(object):
 
-    deck = Deck()
+
+    def __init__(self):
+        self.deck = Deck()
+        self.dealer = Player()
+        self.player = Player()
+
+        self.turn = 0  # even is dealer, odd is player
+
+    def pick__random_card(self, deck):
+        """
+        :param deck: A Deck object
+        :return: a randomly selected Card object from deck.
+        """
+        from random import randrange
+        card_index = randrange(0, len(deck))  # pick a random card
+        card = deck[card_index]  # store random card
+        deck.remove(card_index)  # remove that card from the deck
+        return card
+
+    
+
+
 
 
 c = Card('spades', 'king')
