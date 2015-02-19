559e129e1cc544a7833d7bb75898560a4923c4ee
Oxen <larmannjan@gmail.com>
2015-02-19 20:25:33 +0100

    fixed the rank print on the card of the "10" cards, it used to be just "1"

--- print_card.py
+++ print_card.py
@@ -13,20 +13,26 @@ def convert_card_to_ascii(*cards):
     lines = [[] for i in range(9)]
 
     for index, card in enumerate(cards):
-        rank = card.rank[0]  # some have a rank of 'King' this changes that to a simple 'K' ("King" doesn't fit)
+        # "King" should be "K" and "10" should still be "10"
+        if card.rank == '10':  # ten is the only one who's rank is 2 char long
+            rank = card.rank
+            space = ''  # if we write "10" on the card that line will be 1 char to long
+        else:
+            rank = card.rank[0]  # some have a rank of 'King' this changes that to a simple 'K' ("King" doesn't fit)
+            space = ' '  # no "10", we use a blank space to will the void
         # get the cards suit in two steps
         suit = suits_name.index(card.suit)
         suit = suits_symbols[suit]
 
         # add the individual card on a line by line basis
         lines[0].append('┌─────────┐')
-        lines[1].append('│{}        │'.format(rank))
+        lines[1].append('│{}{}       │'.format(rank, space))
         lines[2].append('│         │')
         lines[3].append('│         │')
         lines[4].append('│    {}    │'.format(suit))
         lines[5].append('│         │')
         lines[6].append('│         │')
-        lines[7].append('│        {}│'.format(rank))
+        lines[7].append('│       {}{}│'.format(space, rank))
         lines[8].append('└─────────┘')
 
     # result = ''.join(line_1) + '\n' + ''.join(line_2) + '\n' + ''.join(line_3) + '\n' + ''.join(line_4) + '\n' + ''.join(line_5) + '\n' + ''.join(line_6) + '\n' + ''.join(line_7) + '\n' + ''.join(line_8) + '\n' + ''.join(line_9)
